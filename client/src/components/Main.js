ff4400e379e45f0c47ae4b23bae9cdc966283d8c
Tomasz <tomekoleszko@gmail.com>
2022-05-28 17:26:51 +0200

    userProfile update

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,87 +1,117 @@
-import React, {useEffect,useState} from 'react';
+import React, {useState} from 'react';
 import axios from 'axios'
 import Wrapper from "../wrapper/PageBtnContainer";
+import {Link} from "react-router-dom"
 
 const Main = () => {
-    const [dane,ustawDane] = useState('')
+    const [dane, ustawDane] = useState('')
 
-    
+
     const handleLogout = () => {
         localStorage.removeItem("token")
         window.location.reload()
     }
-    
-    const handleReadJson = async (e) => {
-            const url = "http://localhost:8000/api/laureates"
-            const token = localStorage.getItem("token");
-            console.log(token)
-            const { data: res } = await  axios.get(url,{headers:{
-                'Authorization': `Bearer ${token}`
-            }})
-            var json = res['laureates'];
-            console.log(json);
-            var loopData = ''
-            var i;
-            for(i=0; i<json.length; i++){
-                if(json[i].hasOwnProperty('givenName'))
-                loopData += `<li>${json[i].fullName.en}</li>`
-            }
-            ustawDane(loopData)
-           
-}
 
-    const handleStore = async (e) => {
+    const handleReadJson = async () => {
+        const url = "http://localhost:8000/api/laureates"
+        const token = localStorage.getItem("token");
+        console.log(token)
+        const {data: res} = await axios.get(url, {
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        })
+        var json = res['laureates'];
+        console.log(json);
+        var loopData = ''
+        var i;
+        for (i = 0; i < json.length; i++) {
+            if (json[i].hasOwnProperty('givenName'))
+                loopData += `<li>${json[i].fullName.en}</li>`
+        }
+        ustawDane(loopData)
+
+    }
+
+    const handleStore = async () => {
         const url = "http://localhost:8000/api/jsonToDatabase"
         const token = localStorage.getItem("token");
         console.log(token)
-        const { data: res } = await  axios.post(url,{},{headers:{
-            'Authorization': `Bearer ${token}`
-        }})
+        const {data: res} = await axios.post(url, {}, {
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        })
         console.log(res)
-        
+
     }
 
-    const handleReadPrizes = async (e) => {
+    const handleReadPrizes = async () => {
         const url = "http://localhost:8000/api/prizes"
         const token = localStorage.getItem("token");
-        
-        const { data: res } = await  axios.get(url,{headers:{
-            'Authorization': `Bearer ${token}`
-        }})
+
+        const {data: res} = await axios.get(url, {
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        })
         const json = res['nobelPrizes']
         console.log(json);
     }
 
-    const handeStorePrizes = async (e) => {
+    const handeStorePrizes = async () => {
         const url = "http://localhost:8000/api/prizesToDatabase"
         const token = localStorage.getItem("token");
         console.log(token);
-        
-        const { data: res } = await  axios.post(url,{},{headers:{
-            'Authorization': `Bearer ${token}`
-        }})
+
+        const {} = await axios.post(url, {}, {
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        })
     }
+
+    const getUser = async () => {
+        const url = "http://127.0.0.1:8000/api/auth/user-profile"
+        const token = localStorage.getItem("token");
+        const {data: res} = await axios.get(url, {
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        })
+        localStorage.setItem('user', JSON.stringify(res))
+    }
+
 
     return (
         <Wrapper>
-        <div>
-            <nav >
-                <h1>MySite</h1>
-                <button className='btn btn-danger' onClick={handleLogout}>
-                    Wyloguj się</button>
-                <button className='btn btn-container' onClick={handleReadJson}>
-                    ReadLaureates</button>
-                <button className='btn btn-container' onClick={handleStore}>
-                    StoreLaureates</button>
-                <button className='btn btn-container' onClick={handleReadPrizes}>
-                    ReadPrizes</button>
-                <button className='btn btn-container' onClick={handeStorePrizes}>
-                    StorePrizes</button>
-            </nav>
-            <div className='content'>
-                <ul dangerouslySetInnerHTML={{__html: dane}}></ul>
+            <div>
+                <nav>
+                    <h1>MySite</h1>
+                    <button className='btn btn-danger' onClick={handleLogout}>
+                        Wyloguj się
+                    </button>
+                    <button className='btn btn-container' onClick={handleReadJson}>
+                        ReadLaureates
+                    </button>
+                    <button className='btn btn-container' onClick={handleStore}>
+                        StoreLaureates
+                    </button>
+                    <button className='btn btn-container' onClick={handleReadPrizes}>
+                        ReadPrizes
+                    </button>
+                    <button className='btn btn-container' onClick={handeStorePrizes}>
+                        StorePrizes
+                    </button>
+                    <Link to='/user'>
+                        <button className='btn btn-container' onClick={getUser}>Profile</button>
+                    </Link>
+
+                </nav>
+                <div className='content'>
+                    <ul dangerouslySetInnerHTML={{__html: dane}}></ul>
+                </div>
             </div>
-        </div>
         </Wrapper>
     )
 }
---
+++ client/src/components/UserProfile.js
@@ -0,0 +1,79 @@
+import React, {useState} from 'react';
+import axios from 'axios'
+import Wrapper from '../wrapper/DashboardFormPage'
+import FormRow from "./FormRow";
+import {BsFillTrashFill} from 'react-icons/bs';
+
+
+const UserProfile = () => {
+    const user = JSON.parse(localStorage.getItem('user'));
+
+    const [name, setName] = useState(user.name || '')
+    const [email, setEmail] = useState(user.email || '')
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        if (!name || !email) {
+            //show blad
+            return
+        }
+
+
+       updateUser({name,email})
+    }
+
+    const updateUser = async (currentUser) =>{
+        try {
+            const { data } = await axios.patch('/auth/updateUser', currentUser)
+
+            const { user, token } = data
+
+
+            addUserToLocalStorage({ user, token })
+        } catch (error) {
+            if (error.response.status !== 401) {
+                //xd
+            }
+        }
+    }
+
+    const addUserToLocalStorage = ({ user, token }) => {
+        localStorage.setItem('user', JSON.stringify(user))
+        localStorage.setItem('token', token)
+    }
+
+    return (
+        <Wrapper>
+            <form className='form' onSubmit={handleSubmit}>
+                <h3>profile</h3>
+                <div className='form-center'>
+                    <FormRow
+                        type='text'
+                        name='name'
+                        value={name}
+                        handleChange={(e) => setName(e.target.value)}
+                    />
+                    <FormRow
+                        type='email'
+                        name='email'
+                        value={email}
+                        handleChange={(e) => setEmail(e.target.value)}
+                    />
+                    <FormRow
+                        type='password'
+                        name='password'
+                        value={password}
+                        handleChange={(e) => setPassword(e.target.value)}
+                    />
+                    <button className='btn btn-block' type='submit'>
+                        save changes
+                    </button>
+                    <button className='btn btn-danger'>
+                        Delete Account <BsFillTrashFill/>
+                    </button>
+                </div>
+            </form>
+        </Wrapper>
+    )
+}
+export default UserProfile
